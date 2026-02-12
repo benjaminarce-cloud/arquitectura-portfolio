a4c0e666102fa3883ca0d7711743c4489af3d217
benjaminarce-cloud <arceb3013@gmail.com>
2026-02-11 16:27:20 -0800

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        xs: ["12px", { lineHeight: "16px" }],
+        sm: ["14px", { lineHeight: "20px" }],
+        base: ["16px", { lineHeight: "24px" }],
+        lg: ["18px", { lineHeight: "28px" }],
+        xl: ["24px", { lineHeight: "32px" }],
+        "2xl": ["32px", { lineHeight: "40px" }],
+        "3xl": ["48px", { lineHeight: "56px" }],
+        "4xl": ["64px", { lineHeight: "72px" }],
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
